f1aac1fbbfe0e3f9886990b924ecd7e93821d1f9
Alex.Nathanson <nathanson.a@husky.neu.edu>
2018-02-08 18:14:50 -0500

    stuff

--- ex3.js
+++ ex3.js
@@ -1,23 +1,32 @@
-var inc = 0.01;
-var scl = 5;
+var inc = 0.1;
+var scl = 10;
 var cols, rows;
+var zoffset = 0
+
 
 function setup(){
 createCanvas(500, 500);
 cols =floor(width / scl);
-rows = floor(height / scl)
+rows =floor(height / scl);
+frameRate(30);
 }
 function draw(){
+  background(255)
+  map(mouseY, 0, 500, 0, 1)
   var yoffset = 0;
   for (var y = 0; y < rows; y++){
-    var xoffset = 0;
+    var xoffset = mouseY;
+    zoffset = zoffset + 0.001;
     for (var x = 0; x < cols; x++){
       var index =(x + y * width) * 4;
-      var r = noise( xoffset, yoffset) * 255;
+      var r = noise( xoffset, yoffset, zoffset) * 255;
     xoffset += inc;
-     fill(r * random(255));
+     fill(r);
+     frameRate(30);
     rect(x * scl, y * scl, scl, scl);
+
     }
   yoffset += inc; 
+
   }
 }
